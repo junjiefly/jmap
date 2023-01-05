ff8f22181593f88175f1f7ad85b6aa58c096a260
junjiefly <107779959+junjiefly@users.noreply.github.com>
2023-01-05 21:06:52 +0800

    Update jmap.go

--- jmap.go
+++ jmap.go
@@ -1,12 +1,14 @@
 package jmap
 
 const count = 1024
+const mask = count - 1
 type arrayList struct{
   array [count][]int
 }
 
 type JMap struct {
    arraylist []arrList
+  length int
 }
 
 func isPowerOfTwo(x int)bool{
@@ -18,6 +20,14 @@ func NewJMap(length int)*JMap{
    ｝
   jmap :=&JMap{
     arraylist:make([]arrList,length)
+    length:length,
     }
     return jmap
 }
+  
+  
+  func(jmap *JMap)Set(k uint64, v int){
+    idx := k & (jmap.length - 1)
+    bmap := jmap[idx]
+    
+  }
