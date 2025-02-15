8c8202a47cd9a09d5b5efe39f30d60dd26abaa91
junjiefly <107779959+junjiefly@users.noreply.github.com>
2025-02-15 13:32:27 +0800

    update

--- README.md
+++ README.md
@@ -14,17 +14,17 @@
 
 # benchMark
 
-    j-map ready! len: 10000000  
-    goMap ready! len: 10000000  
-    goos: linux  
-    goarch: amd64  
-    pkg: jmap  
-    cpu: Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20GHz  
-    BenchmarkJMapSet-40      	1000000000	         0.0000019 ns/op  
-    BenchmarkJMapGet-40      	1000000000	         0.0000017 ns/op  
-    BenchmarkJMapDel-40      	1000000000	         0.0000015 ns/op  
-    BenchmarkMapSet-40       	1000000000	         0.0000019 ns/op  
-    BenchmarkMapGet-40       	1000000000	         0.0000020 ns/op  
-    BenchmarkMapDelete-40    	1000000000	         0.0000027 ns/op  
-    PASS  
-    ok  	jmap	5.919s
+    j-map ready! len: 0
+    goMap ready! len: 0
+    goos: linux
+    goarch: amd64
+    pkg: github.com/junjiefly/jmap
+    cpu: Intel(R) Core(TM) i3-8100T CPU @ 3.10GHz
+    BenchmarkJMapSet-4      	14406464	        85.08 ns/op	      34 B/op	       0 allocs/op
+    BenchmarkJMapGet-4      	28466576	        47.05 ns/op	       0 B/op	       0 allocs/op
+    BenchmarkJMapDelete-4   	17903854	        62.91 ns/op	       0 B/op	       0 allocs/op
+    BenchmarkMapSet-4       	15370224	       218.7 ns/op	      86 B/op	       0 allocs/op
+    BenchmarkMapGet-4       	28448533	        61.85 ns/op	       0 B/op	       0 allocs/op
+    BenchmarkMapDelete-4    	18742928	        70.59 ns/op	       0 B/op	       0 allocs/op
+    PASS
+    ok  	github.com/junjiefly/jmap	11.371s
--- jmap_test.go
+++ jmap_test.go
@@ -2,54 +2,59 @@ package jmap
 
 import (
 	"fmt"
-	"math/rand"
 	"testing"
 )
 
 var jMap *JMap
 
-var needleCount = 10000000
+var needleCount = 1000000
 
 var mmap map[uint64]int
 
 func TestMain(m *testing.M) {
-	jMap = NewJMap(1024)
-	mmap = make(map[uint64]int,1024)
-	for i := 0; i < needleCount; i++ {
-		jMap.Set(uint64(i), i)
-		mmap[uint64(i)] = i
-	}
+	jMap = NewJMap(1024) //length has to be the power of 2
+	mmap = make(map[uint64]int,needleCount)
+	//for i := 0; i < needleCount; i++ {
+	//	jMap.Set(uint64(i), i)
+	//	mmap[uint64(i)] = i
+	//}
 	fmt.Println("j-map ready! len:", jMap.count)
 	fmt.Println("goMap ready! len:", len(mmap))
 	m.Run()
 }
 
 func BenchmarkJMapSet(b *testing.B) {
-	k := rand.Intn(needleCount)
-	jMap.Set(uint64(k), 0)
+	for i:=0;i<b.N;i++{
+		jMap.Set(uint64(i), 0)
+	}
 }
 
 func BenchmarkJMapGet(b *testing.B) {
-	k := rand.Intn(needleCount)
-	_, _ = jMap.Get(uint64(k))
+	for i:=0;i<b.N;i++{
+		_, _ = jMap.Get(uint64(i))
+	}
 }
 
 func BenchmarkJMapDelete(b *testing.B) {
-	k := rand.Intn(needleCount)
-	_ = jMap.Delete(uint64(k))
+	for i:=0;i<b.N;i++{
+		_ = jMap.Delete(uint64(i))
+	}
 }
 
 func BenchmarkMapSet(b *testing.B) {
-	k := rand.Intn(needleCount)
-	mmap[uint64(k)] = 0
+	for i:=0;i<b.N;i++{
+		mmap[uint64(i)] = 0
+	}
 }
 
 func BenchmarkMapGet(b *testing.B) {
-	k := rand.Intn(needleCount)
-	_, _ = mmap[uint64(k)]
+	for i:=0;i<b.N;i++{
+		_, _ = mmap[uint64(i)]
+	}
 }
 
 func BenchmarkMapDelete(b *testing.B) {
-	k := rand.Intn(needleCount)
-	delete(mmap, uint64(k))
+	for i:=0;i<b.N;i++{
+		delete(mmap, uint64(i))
+	}
 }
